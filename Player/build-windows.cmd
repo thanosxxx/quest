29411e3d31f71c65456925cef814255660f131b1
Alex Warren <alex@textadventures.co.uk>
2015-12-26 18:22:23 +0000

    Build Windows QuestPlayer.exe

---
+++ Player/build-windows.cmd
@@ -0,0 +1,4 @@
+md ..\Editor\Quest\bin\x86\Debug
+md ..\Editor\Quest\bin\x86\Release
+robocopy QuestPlayer-win32-ia32 ..\Editor\Quest\bin\x86\Debug /s
+robocopy QuestPlayer-win32-ia32 ..\Editor\Quest\bin\x86\Release /s
