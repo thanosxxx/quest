e6eca17d74922272c6425fc358eee3ebe0c949ee
Alex Warren <alex@textadventures.co.uk>
2015-12-28 17:02:44 +0000

    Move menu to main.js

--- Player/main.js
+++ Player/main.js
@@ -5,15 +5,12 @@ var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 var storage = require('./storage');
 
-// Report crashes to our server.
-require('crash-reporter').start();
-
 var argv = process.argv;
 
 var openFile;
 
 if (process.platform !== 'darwin') {
-  openFile = process.argv[1];
+    openFile = process.argv[1];
 }
 
 app.on('open-file', function (event, path) {
@@ -29,57 +26,61 @@ app.on('open-file', function (event, path) {
 var mainWindow = null;
 
 var init = function() {
-  var lastWindowState = storage.get('lastWindowState');
-  if (lastWindowState === null) {
-    lastWindowState = {
-      width: 1200,
-      height: 600,
-      maximized: false 
-    }; 
-  }
-  
-  mainWindow = new BrowserWindow({
-    x: lastWindowState.x,
-    y: lastWindowState.y,
-    width: lastWindowState.width, 
-    height: lastWindowState.height,
-    //icon: __dirname + '/quest.png'
-  });
-  
-  if (lastWindowState.maximized) {
-    mainWindow.maximize();
-  }
-
-  mainWindow.openFile = openFile;
-
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/start.html');
-  
-  mainWindow.on('close', function () {
-    var bounds = mainWindow.getBounds(); 
-    storage.set('lastWindowState', {
-      x: bounds.x,
-      y: bounds.y,
-      width: bounds.width,
-      height: bounds.height,
-      maximized: mainWindow.isMaximized()
+    var lastWindowState = storage.get('lastWindowState');
+    if (lastWindowState === null) {
+        lastWindowState = {
+        width: 1200,
+        height: 600,
+        maximized: false 
+        }; 
+    }
+    
+    mainWindow = new BrowserWindow({
+        x: lastWindowState.x,
+        y: lastWindowState.y,
+        width: lastWindowState.width, 
+        height: lastWindowState.height,
+        //icon: __dirname + '/quest.png'
     });
-  });
+    
+    if (lastWindowState.maximized) {
+        mainWindow.maximize();
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    mainWindow.openFile = openFile;
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/start.html');
+    
+    mainWindow.on('close', function () {
+        var bounds = mainWindow.getBounds(); 
+        storage.set('lastWindowState', {
+        x: bounds.x,
+        y: bounds.y,
+        width: bounds.width,
+        height: bounds.height,
+        maximized: mainWindow.isMaximized()
+        });
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+    
+    var Menu = require('electron').Menu;
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 };
 
 // Quit when all windows are closed, except on OS X.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 // On OS X, this is called when the app is running in the Dock with no open windows.
@@ -88,3 +89,172 @@ app.on('activate-with-no-open-windows', init);
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', init);
+
+var template = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open...',
+                accelerator: 'CmdOrCtrl+O',
+                click: function () {
+                    
+                }
+            },
+            {
+                label: 'Save',
+                accelerator: 'CmdOrCtrl+S',
+                click: function () {
+                    
+                }
+            },
+            {
+                label: 'Save As...',
+                accelerator: 'CmdOrCtrl+Shift+S',
+                click: function () {
+                    
+                }
+            },
+        ]
+      },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Select All',
+                accelerator: 'CmdOrCtrl+A',
+                role: 'selectall'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'CmdOrCtrl+C',
+                role: 'copy'
+            },
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.reload();
+                }
+            },
+            {
+                label: 'Toggle Full Screen',
+                accelerator: (function() {
+                if (process.platform == 'darwin')
+                    return 'Ctrl+Command+F';
+                else
+                    return 'F11';
+                })(),
+                click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (function() {
+                if (process.platform == 'darwin')
+                    return 'Alt+Command+I';
+                else
+                    return 'Ctrl+Shift+I';
+                })(),
+                click: function(item, focusedWindow) {
+                if (focusedWindow)
+                    focusedWindow.toggleDevTools();
+                }
+            },
+        ]
+    },
+    {
+        label: 'Window',
+        role: 'window',
+        submenu: [
+            {
+                label: 'Minimize',
+                accelerator: 'CmdOrCtrl+M',
+                role: 'minimize'
+            },
+            {
+                label: 'Close',
+                accelerator: 'CmdOrCtrl+W',
+                role: 'close'
+            },
+        ]
+    },
+    {
+        label: 'Help',
+        role: 'help',
+        submenu: [
+            {
+                label: 'Documentation',
+                click: function() { require('electron').shell.openExternal('http://docs.textadventures.co.uk/quest/') }
+            },
+        ]
+    },
+];
+
+var name = 'Quest Player';
+
+var appMenuTemplate = {
+    label: name,
+    submenu: [
+        {
+            label: 'About ' + name,
+            role: 'about'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Services',
+            role: 'services',
+            submenu: []
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Hide ' + name,
+            accelerator: 'Command+H',
+            role: 'hide'
+        },
+        {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            role: 'hideothers'
+        },
+        {
+            label: 'Show All',
+            role: 'unhide'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+        },
+    ]
+};
+
+if (process.platform == 'darwin') {
+    template.unshift(appMenuTemplate);
+    // Window menu.
+    template[3].submenu.push(
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Bring All to Front',
+            role: 'front'
+        }
+    );
+}
+
