885a170e9b62b85e3e71253654a753de364b3797
Alex Warren <alex@textadventures.co.uk>
2016-03-12 10:49:56 +0000

    Gulpfile task to create dist folder

--- Player/gulpfile.js
+++ Player/gulpfile.js
@@ -5,7 +5,7 @@ var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var tsProject = ts.createProject('./tsconfig.json');
 
-gulp.task('asl4', function() {
+gulp.task('asl4', function () {
     gulp.src('./node_modules/gulp-babel/node_modules/babel-core/browser-polyfill.min.js')
         .pipe(gulp.dest('.'));
       
@@ -17,3 +17,30 @@ gulp.task('asl4', function() {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('.'));
 });
+
+gulp.task('lib', function () {
+    return gulp.src('lib/**/*')
+        .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('scripts/**/*')
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('ui', function () {
+    return gulp.src('ui/**/*')
+        .pipe(gulp.dest('dist/ui'));
+});
+
+gulp.task('files', function () {
+    return gulp.src([
+        '*.html',
+        '*.asl',
+        '*.aslx',
+        '*.cas',
+        '*.js'
+    ]).pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lib', 'scripts', 'ui', 'files']);
